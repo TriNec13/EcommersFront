8f81e9cdc7b3c18ddef9f26cfe59923d28d88b6d
Ivan Valero <126502492+Ivan-Nicolas-Valero-FT-36B@users.noreply.github.com>
2023-06-21 22:07:51 -0300

    actualiza localstore

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -19,6 +19,7 @@ const Nav = () => {
   const handleLogout = () => {
     // Aquí puedes realizar la lógica de cierre de sesión, como limpiar las variables de sesión, etc.
     setIsLoggedIn(false);
+    localStorage.setItem('isLoggedIn', false);
     navigate("/login"); // Redireccionar al usuario a la página de inicio de sesión
     // logoutUser();
   };
