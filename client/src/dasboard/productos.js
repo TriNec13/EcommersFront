90d06db267ae5ef013a3bd74b1c5b34f95a27edd
Ivan Valero <126502492+Ivan-Nicolas-Valero-FT-36B@users.noreply.github.com>
2023-06-22 16:33:10 -0300

    Update productos.js

--- client/src/dasboard/productos.js
+++ client/src/dasboard/productos.js
@@ -1,172 +1,51 @@
 export const data1 = [
     {
       id: 1,
-      name: "EnergiBlast ",
-      category: "Ropa",
-      precio: 19.99,
+      name: "PaintPro Studio",
+      category: "Arte Digital",
+      precio: 150,
       disponible: true,
-      imagen: "https://ejemplo.com/camiseta-algodon.jpg"
+      imagen: "https://www.clipstudio.net/view/ab_test/202207/img/02.jpg"
     },
     {
       id: 2,
-      name: "FreshGlow",
-      category: "Calzado",
-      precio: 59.99,
+      name: "ProStylus",
+      category: "Arte Digital",
+      precio: 129,
       disponible: true,
-      imagen: "https://img.freepik.com/fotos-premium/par-zapatillas-deportivas-azules-sobre-fondo-blanco-aislado_77190-7490.jpg"
+      imagen: "https://res.cloudinary.com/dq9qehsik/image/upload/v1687367952/productos/ProStylus_z13eu8.jpg"
     },
     {
       id: 3,
-      name: "PowerFlex",
-      category: "Componentes",
-      precio: 39.99,
+      name: "BrushMaster",
+      category: "Arte Digital",
+      precio: 199,
       disponible: true,
-      imagen: "https://m.media-amazon.com/images/I/61fesY6jynL._AC_UF894,1000_QL80_.jpg"
+      imagen: "https://res.cloudinary.com/dq9qehsik/image/upload/v1687367948/productos/BrushMaster_psbnhw.png"
     },
     {
         id: 4,
-        name: "PureLife ",
-        category: "Ropa",
-        precio: 39.99,
+        name: "SecureShield",
+        category: "Antivirus",
+        precio: 49,
         disponible: true,
-        imagen: "https://www.shutterstock.com/image-photo/women-sandals-shoes-on-white-260nw-2135005389.jpg"
+        imagen: "https://res.cloudinary.com/dq9qehsik/image/upload/v1687367952/productos/SecureShield_quzpzx.jpg"
       },
       {
         id: 5,
-        name: "QuickClean ",
-        category: "Ropa",
-        precio: 39.99,
+        name: "FilmCraft Studio ",
+        category: "Edición de Video",
+        precio: 399,
         disponible: true,
-        imagen: "https://www.moov.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dwf2f7398d/products/NI_SX7676-964/NI_SX7676-964-1.JPG"
+        imagen: "https://res.cloudinary.com/dq9qehsik/image/upload/v1687367949/productos/FilmCraft_Studio_rjyd1j.jpg"
       },
       {
         id: 6,
-        name: "FitFuel ",
-        category: "Componentes",
-        precio: 19.99,
+        name: "BrushMaster",
+        category: "Arte Digital",
+        precio: 199,
         disponible: true,
-        imagen: "https://ejemplo.com/camiseta-algodon.jpg"
-      },
-      {
-        id: 7,
-        name: "ZenSleep ",
-        category: "Calzado",
-        precio: 59.99,
-        disponible: true,
-        imagen: "https://img.freepik.com/fotos-premium/par-zapatillas-deportivas-azules-sobre-fondo-blanco-aislado_77190-7490.jpg"
-      },
-      {
-        id: 8,
-        name: "SmartGrip ",
-        category: "Ropa",
-        precio: 39.99,
-        disponible: true,
-        imagen: "https://m.media-amazon.com/images/I/61fesY6jynL._AC_UF894,1000_QL80_.jpg"
-      },
-      {
-          id: 9,
-          name: "NatureBlend ",
-          category: "Componentes",
-          precio: 39.99,
-          disponible: true,
-          imagen: "https://www.shutterstock.com/image-photo/women-sandals-shoes-on-white-260nw-2135005389.jpg"
-        },
-        {
-          id: 10,
-          name: "HappyPaws",
-          category: "Ropa",
-          precio: 39.99,
-          disponible: true,
-          imagen: "https://www.moov.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dwf2f7398d/products/NI_SX7676-964/NI_SX7676-964-1.JPG"
-        },
-        {
-            id: 11,
-            name: "AquaSplash ",
-            category: "Ropa",
-            precio: 19.99,
-            disponible: true,
-            imagen: "https://ejemplo.com/camiseta-algodon.jpg"
-          },
-          {
-            id: 12,
-            name: "SparkleShine",
-            category: "Componentes",
-            precio: 59.99,
-            disponible: true,
-            imagen: "https://img.freepik.com/fotos-premium/par-zapatillas-deportivas-azules-sobre-fondo-blanco-aislado_77190-7490.jpg"
-          },
-          {
-            id: 13,
-            name: "MegaPower",
-            category: "Ropa",
-            precio: 39.99,
-            disponible: true,
-            imagen: "https://m.media-amazon.com/images/I/61fesY6jynL._AC_UF894,1000_QL80_.jpg"
-          },
-          {
-              id: 14,
-              name: "FreshAir ",
-              category: "Ropa",
-              precio: 39.99,
-              disponible: true,
-              imagen: "https://www.shutterstock.com/image-photo/women-sandals-shoes-on-white-260nw-2135005389.jpg"
-            },
-            {
-              id: 15,
-              name: "ActiveStride ",
-              category: "Componentes",
-              precio: 39.99,
-              disponible: true,
-              imagen: "https://www.moov.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dwf2f7398d/products/NI_SX7676-964/NI_SX7676-964-1.JPG"
-            },
-            {
-              id: 16,
-              name: "SleepWell ",
-              category: "Ropa",
-              precio: 19.99,
-              disponible: true,
-              imagen: "https://ejemplo.com/camiseta-algodon.jpg"
-            },
-            {
-              id: 17,
-              name: "SuperSweep ",
-              category: "Calzado",
-              precio: 59.99,
-              disponible: true,
-              imagen: "https://img.freepik.com/fotos-premium/par-zapatillas-deportivas-azules-sobre-fondo-blanco-aislado_77190-7490.jpg"
-            },
-            {
-              id: 18,
-              name: "TimelessBeauty",
-              category: "Componentes",
-              precio: 39.99,
-              disponible: true,
-              imagen: "https://m.media-amazon.com/images/I/61fesY6jynL._AC_UF894,1000_QL80_.jpg"
-            },
-            {
-                id: 19,
-                name: "MightyBite ",
-                category: "Ropa",
-                precio: 39.99,
-                disponible: true,
-                imagen: "https://www.shutterstock.com/image-photo/women-sandals-shoes-on-white-260nw-2135005389.jpg"
-              },
-              {
-                id: 20,
-                name: "LightWave ",
-                category: "Ropa",
-                precio: 39.99,
-                disponible: true,
-                imagen: "https://www.moov.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dwf2f7398d/products/NI_SX7676-964/NI_SX7676-964-1.JPG"
-              },
-    // Agrega los Componentes productos aquí
-    {
-      id: 50,
-      name: "HappyHome ",
-      category: "Accesorios",
-      precio: 14.99,
-      disponible: true,
-      imagen: "https://m.media-amazon.com/images/I/51ybpPr5TcL._AC_UY350_.jpg"
-    }
+        imagen: "https://res.cloudinary.com/dq9qehsik/image/upload/v1687367948/productos/BrushMaster_psbnhw.png"
+      }
   ];
   
